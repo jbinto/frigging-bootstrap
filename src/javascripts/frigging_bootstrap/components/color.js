9bdc26f0ec6567bfca75900f92b2091a4b8603b7
Jason Stathopulos <jstathopulos@touchbistro.com>
2015-08-14 15:57:40 -0400

    Add Colour Picker Container
    Add a colour picker container which will contain the different
    components of the colour since each component will be use to select a
    colour.

---
+++ src/javascripts/frigging_bootstrap/components/color.js
@@ -0,0 +1,116 @@
+let React = require("react")
+let Colr = require('colr')
+let {errorList, sizeClassNames, formGroupCx, label} = require("../util.js")
+let {div, input, img} = React.DOM
+let cx = require("classnames")
+
+let colorMap = React.createFactory(require("./color/map"))
+
+export default class extends React.Component {
+
+  static displayName = "Frig.friggingBootstrap.Color"
+
+  static defaultProps = Object.assign(require("../default_props.js"), {
+    color: "#000000",
+  })
+
+  propTypes: {
+    color: React.PropTypes.string,
+  }
+
+  state = this._getStateFrom(this.props.color)
+
+  // compare props against state using hex strings
+  // only use the new props if the color is different
+  // this prevents data loss when converting between RGB and HSV
+  componentWillReceiveProps(nextProps) {
+    let nextColor = nextProps.color.toLowerCase()
+    let currentColor = Colr.fromHsvObject(this.state.hsv).toHex()
+
+    if(nextColor !== currentColor) {
+      this.setState(this._getStateFrom(nextProps.color))
+    }
+  }
+
+  // generate state object from a hex string
+  _getStateFrom(color) {
+    color = Colr.fromHex(color)
+    return {
+      color: color,
+      origin: color.clone(),
+      hsv: color.toRawHsvObject(),
+    }
+  }
+
+  // replace current color with another one
+  _loadColor(color) {
+    this.setState(this._getStateFrom(color))
+    this.props.onChange(Colr.fromHex(color))
+  }
+
+  // update the current color using the raw hsv values
+  _update() {
+    let color = Colr.fromHsvObject(this.state.hsv)
+    this.setState({ color: color })
+    this.props.onChange(color)
+  }
+
+  // set the hue
+  _setHue(hue) {
+    this.state.hsv.h = hue
+    this._update()
+  }
+
+  // set the saturation
+  _setSaturation(saturation) {
+    this.state.hsv.s = saturation
+    this._update()
+  }
+
+  // set the value
+  _setValue(value) {
+    this.state.hsv.v = value
+    this._update()
+  }
+
+  // set the saturation and the value
+  _setSaturationAndValue(saturation, value) {
+    this.state.hsv.s = saturation
+    this.state.hsv.v = value
+    this._update()
+  }
+
+  _getBackgroundHue() {
+    return Colr.fromHsv(this.state.hsv.h, 100, 100).toHex()
+  }
+
+  _input() {
+    let hue = this._getBackgroundHue()
+    let luminosity = this.state.color.toGrayscale()
+
+    return colorMap({
+      x: this.state.hsv.s,
+      y: this.state.hsv.v,
+      max: 100,
+      backgroundColor: hue,
+      className: cx({
+        dark: luminosity <= 128,
+        light: luminosity > 128,
+      }),
+      onChange: this._setSaturationAndValue,
+    })
+  }
+
+  render() {
+    return div({className: cx(sizeClassNames(this.props))},
+      div({className: formGroupCx(this.props)},
+        label(this.props),
+        div({className: "controls"},
+          this._input(),
+        ),
+        errorList(this.props.errors),
+      ),
+    )
+  }
+
+}
