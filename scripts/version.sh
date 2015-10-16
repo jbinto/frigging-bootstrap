fa52fc1ea4f985b13b3f232a4fc1a1ab5277c09b
D1plo1d <d1plo1d@d1plo1d.com>
2015-10-16 15:00:20 -0400

    Fixing version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,7 @@
 echo ""
 echo "FRIGGING BOOTSTRAP: UPDATING BOWER VERSION"
 echo "======================================================="
-VERSION=`npm version|grep -e "frigging-bootstrap: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
+VERSION=`npm version|grep -e "frigging-bootstrap[^:]*: '[0-9\.]*"|grep -E [0-9\.]+|grep -oE "[0-9\.]+"`
 sed -i.bak -e "s/\"version\": \"[^\"]*\"/\"version\": \"$VERSION\"/g" bower.json
 rm bower.json.bak
 git add bower.json
