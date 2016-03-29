3ea9c1dcc29a75a1720fd5b03b2c9638211f67ce
Rob Gilson <thatotherdude@gmail.com>
2016-03-29 12:58:57 -0400

    Change how themes are loaded

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,9 +1,27 @@
-require("../stylesheets/index.styl")
+import "../stylesheets/index.styl"
+import Checkbox from "./components/checkbox.js"
+import Color from "./components/color.js"
+import FormErrorList from "./components/errors.js"
+import File from "./components/file.js"
+import Form from "./components/form.js"
+import Input from "./components/input.js"
+import Number from "./components/number.js"
+import Select from "./components/select.js"
+import Submit from "./components/submit.js"
+import Switch from "./components/switch.js"
+import Text from "./components/text.js"
+import Timepicker from "./components/timepicker.js"
 
 module.exports = {
-
-  component(name) {
-    return require(`./components/${name}.js`)
-  },
-
+  Checkbox,
+  Color,
+  FormErrorList,
+  File,
+  Input,
+  Number,
+  Select,
+  Submit,
+  Switch,
+  Text,
+  Timepicker,
 }
