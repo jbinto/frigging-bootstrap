94779ce66f96b8106e3062b272db0452e0215b09
Jesse Buchanan <hello@jessebuchanan.ca>
2016-03-30 12:49:40 -0400

    add PropTypes to Input

--- src/javascripts/components/input.js
+++ src/javascripts/components/input.js
@@ -12,6 +12,29 @@ import {
 export default class Input extends React.Component {
   displayName = "FriggingBootstrap.Input"
 
+  static propTypes = {
+    inputHtml: React.PropTypes.shape({
+      type: React.PropTypes.string.isRequired,
+    }).isRequired,
+    inputWrapper: React.PropTypes.func.isRequired,
+
+    valueLink: React.PropTypes.shape({
+      value: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number,
+        React.PropTypes.bool,
+      ]),
+      requestChange: React.PropTypes.func,
+    }).isRequired,
+
+    className: React.PropTypes.string,
+    prefix: React.PropTypes.string,
+    suffix: React.PropTypes.string,
+
+    saved: React.PropTypes.bool,
+    errors: React.PropTypes.array,
+  }
+
   static defaultProps = Object.assign(require("../default_props.js"), {
     // Bootstrap input addon texts
     prefix: undefined,
