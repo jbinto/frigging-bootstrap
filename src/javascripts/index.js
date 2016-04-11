f546f01323a81a009d216ef09956b6a69c1d8541
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-04-11 15:23:38 -0400

    Lint Index.JS

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,18 +1,18 @@
-import "../stylesheets/index.styl"
-import Checkbox from "./components/checkbox.js"
-import Color from "./components/color.js"
-import FormErrorList from "./components/errors.js"
-import File from "./components/file.js"
-import Form from "./components/form.js"
-import Input from "./components/input.js"
-import Number from "./components/number.js"
-import Select from "./components/select.js"
-import Submit from "./components/submit.js"
-import Switch from "./components/switch.js"
-import Text from "./components/text.js"
-import Timepicker from "./components/timepicker.js"
+import '../stylesheets/index.styl'
+import Checkbox from './components/checkbox.js'
+import Color from './components/color.js'
+import FormErrorList from './components/errors.js'
+import File from './components/file.js'
+import Form from './components/form.js'
+import Input from './components/input.js'
+import Number from './components/number.js'
+import Select from './components/select.js'
+import Submit from './components/submit.js'
+import Switch from './components/switch.js'
+import Text from './components/text.js'
+import Timepicker from './components/timepicker.js'
 
-let {errorList, saveList} = require("./util")
+const { errorList, saveList } = require('./util')
 
 module.exports = {
   Checkbox,
