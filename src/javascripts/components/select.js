cb7f7dad2d72b206560548f3d569097cdb1432b2
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-04-11 16:35:46 -0400

    Lint Select Component

--- src/javascripts/components/select.js
+++ src/javascripts/components/select.js
@@ -1,24 +1,44 @@
-let React = require("react")
-let cx = require("classnames")
-
-let {
+import React from 'react'
+import cx from 'classnames'
+import {
   saveList,
   errorList,
   sizeClassNames,
   formGroupCx,
   label,
-} = require("../util.js")
-let select = React.createFactory(require("frig").ValueLinkedSelect)
+} from '../util.js'
+
+import { ValueLinkedSelect } from 'frig'
 
 export default class Select extends React.Component {
-  displayName = "FriggingBootstrap.Select"
+  static displayName = 'FriggingBootstrap.Select'
 
-  static defaultProps = require("../default_props.js")
+  static defaultProps = Object.assign({}, require('../default_props.js'), {
+    options: {},
+  })
+
+  static propTypes = {
+    inputHtml: React.PropTypes.shape({
+      type: React.PropTypes.string.isRequired,
+    }).isRequired,
+    options: React.PropTypes.object,
+    valueLink: React.PropTypes.shape({
+      value: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number,
+        React.PropTypes.bool,
+      ]),
+      requestChange: React.PropTypes.func,
+    }).isRequired,
+
+    saved: React.PropTypes.bool,
+    errors: React.PropTypes.array,
+  }
 
   _inputHtml() {
     return Object.assign({}, this.props.inputHtml, {
-      key: "input",
-      className: "form-control",
+      key: 'input',
+      className: 'form-control',
       valueLink: this.props.valueLink,
       options: this.props.options,
     })
@@ -28,9 +48,11 @@ export default class Select extends React.Component {
     return (
       <div className={cx(sizeClassNames(this.props))}>
         <div className={formGroupCx(this.props)}>
-          {label(this.props, {className:""})}
+          {
+            label(this.props, { className: '' })
+          }
           <div className="controls">
-            {select(this._inputHtml())}
+            <ValueLinkedSelect {...this._inputHtml()} />
             {errorList(this.props.errors)}
           </div>
           {saveList(this.props.saved)}
