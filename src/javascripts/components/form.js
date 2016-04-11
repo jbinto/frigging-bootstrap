daf39558897b5c7af00825c87a16e5c930d6ba06
Jason Stathopulos <jstathopulos@touchbistro.com>
2016-04-11 16:52:59 -0400

    Lint Form Component

--- src/javascripts/components/form.js
+++ src/javascripts/components/form.js
@@ -1,29 +1,39 @@
-let React = require("react")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DefaultProps from '../default_props.js'
 
 export default class Form extends React.Component {
-  displayName = "FriggingBootstrap.Form"
+  static displayName = 'FriggingBootstrap.Form'
 
   static defaultProps = {
-    layout: require("../default_props.js").layout,
+    layout: DefaultProps.layout,
+  }
+
+  static propTypes = {
+    formHtml: React.PropTypes.shape({
+      className: React.PropTypes.string,
+    }),
+    layout: React.PropTypes.string,
+    children: React.PropTypes.arrayOf(React.PropTypes.element),
   }
 
   _formHtml() {
-    let className = this.props.layout ? `form-${this.props.layout}` : ""
+    const className = this.props.layout ? `form-${this.props.layout}` : ''
     return Object.assign({}, this.props.formHtml, {
-      ref: "form",
-      className: `${this.props.formHtml.className||""} ${className}`.trim(),
+      ref: 'form',
+      className: `${this.props.formHtml.className || ''} ${className}`.trim(),
     })
   }
 
   formData() {
-    let formElement = ReactDOM.findDOMNode(this.refs.form)
+    const formElement = ReactDOM.findDOMNode(this.refs.form)
     return new FormData(formElement)
   }
 
   render() {
     return (
       <form {...this._formHtml()}>
-        {this.props.children}
+        { this.props.children }
       </form>
     )
   }
